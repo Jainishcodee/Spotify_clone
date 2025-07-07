4bec4f938d13adcba3cac1eaf2f584d8d501ef6e
Jainish Shah <jainishshah356@gmail.com>
2025-07-07 22:14:33 +0530

    Update local_notification_service.dart

--- lib/local_notification_service.dart
+++ lib/local_notification_service.dart
@@ -1,1 +1,41 @@
 
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static Future<void> init() async {
+    const AndroidInitializationSettings androidSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings initSettings = InitializationSettings(
+      android: androidSettings,
+    );
+
+    await _notificationsPlugin.initialize(initSettings,
+        onDidReceiveNotificationResponse: (response) {
+      // TODO: Handle notification tap
+    });
+  }
+
+  static Future<void> showNotification({
+    required int id,
+    required String title,
+    required String body,
+  }) async {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+      'default_channel_id',
+      'Default',
+      channelDescription: 'This is the default notification channel',
+      importance: Importance.high,
+      priority: Priority.high,
+    );
+
+    const NotificationDetails details =
+        NotificationDetails(android: androidDetails);
+
+    await _notificationsPlugin.show(id, title, body, details);
+  }
+}
